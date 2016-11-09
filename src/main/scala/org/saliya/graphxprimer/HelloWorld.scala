994d1c9c8c7511458c27b190d44c5f946d60fb6d
esaliya <esaliya@gmail.com>
2016-11-09 15:57:02 -0500

    just a commit

---
+++ src/main/scala/org/saliya/graphxprimer/HelloWorld.scala
@@ -0,0 +1,11 @@
+package org.saliya.graphxprimer
+
+/**
+  * Saliya Ekanayake on 11/9/16.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit ={
+    println("Hello, world!")
+  }
+
+}
