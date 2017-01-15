c78f46fd7392eff78f6f0805cb96f798882a1bfc
Saliya Ekanayake <esaliya@gmail.com>
2017-01-15 18:11:06 -0500

    page rank with spit iteration count

---
+++ src/main/scala-2.10/org/saliya/graphxprimer/livejournal/PageRankCountIterations.scala
@@ -0,0 +1,183 @@
+package org.saliya.graphxprimer.livejournal
+
+import org.apache.spark.graphx.{EdgeDirection, EdgeTriplet, Pregel, _}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
+/**
+  * Saliya Ekanayake on 1/7/17.
+  *
+  * Direct copy of the Spark's PageRank implementation
+  * Modified to count iterations
+  */
+object PageRankCountIterations {
+  def main(args: Array[String]): Unit = {
+    val taskType = args(0)
+    val fname = args(1)
+    val optionsList = args.drop(2).map { arg =>
+      arg.dropWhile(_ == '-').split('=') match {
+        case Array(opt, v) => (opt -> v)
+        case _ => throw new IllegalArgumentException("Invalid argument: " + arg)
+      }
+    }
+    val options = mutable.Map(optionsList: _*)
+
+    val conf = new SparkConf()
+    GraphXUtils.registerKryoClasses(conf)
+
+    val numEPart = options.remove("numEPart").map(_.toInt).getOrElse {
+      println("Set the number of edge partitions using --numEPart.")
+      sys.exit(1)
+    }
+    val partitionStrategy: Option[PartitionStrategy] = options.remove("partStrategy")
+      .map(PartitionStrategy.fromString(_))
+    val edgeStorageLevel = options.remove("edgeStorageLevel")
+      .map(StorageLevel.fromString(_)).getOrElse(StorageLevel.MEMORY_ONLY)
+    val vertexStorageLevel = options.remove("vertexStorageLevel")
+      .map(StorageLevel.fromString(_)).getOrElse(StorageLevel.MEMORY_ONLY)
+
+    taskType match {
+      case "pagerank" =>
+        val tol = options.remove("tol").map(_.toFloat).getOrElse(0.001F)
+        val outFname = options.remove("output").getOrElse("")
+        val numIterOpt = options.remove("numIter").map(_.toInt)
+
+        options.foreach {
+          case (opt, _) => throw new IllegalArgumentException("Invalid option: " + opt)
+        }
+
+        println("======================================")
+        println("|             PageRank               |")
+        println("======================================")
+
+        val sc = new SparkContext(conf.setAppName("PageRank(" + fname + ")"))
+
+        val unpartitionedGraph = GraphLoader.edgeListFile(sc, fname,
+          numEdgePartitions = numEPart,
+          edgeStorageLevel = edgeStorageLevel,
+          vertexStorageLevel = vertexStorageLevel).cache()
+        val graph = partitionStrategy.foldLeft(unpartitionedGraph)(_.partitionBy(_))
+
+        val vertexCount = graph.vertices.count
+        println("GRAPHX: Number of vertices " + vertexCount)
+        println("GRAPHX: Number of edges " + graph.edges.count)
+
+        val pr = (numIterOpt match {
+//          case Some(numIter) => PageRank.run(graph, numIter)
+          case None => runUntilConvergence(graph, tol)
+        }).vertices.cache()
+
+        println("GRAPHX: Total rank: " + pr.map(_._2._1).reduce(_ + _))
+        pr.foreach(v => println("*** vertex:" + v._1 + " itr: " + v._2._2))
+        println("GRAPHX: Avg iterations: " + (pr.map(_._2._2).reduce(_ + _) * 1.0 / vertexCount))
+
+
+        if (!outFname.isEmpty) {
+//          logWarning("Saving pageranks of pages to " + outFname)
+          pr.map { case (id, r) => id + "\t" + r }.saveAsTextFile(outFname)
+        }
+
+        sc.stop()
+      case _ =>
+        println("Invalid task type.")
+    }
+  }
+
+  def runUntilConvergence[VD: ClassTag, ED: ClassTag](
+                                                       graph: Graph[VD, ED], tol: Double, resetProb: Double = 0.15): Graph[(Double,Int), Double] =
+  {
+    runUntilConvergenceWithOptions(graph, tol, resetProb)
+  }
+
+  /**
+    * Run a dynamic version of PageRank returning a graph with vertex attributes containing the
+    * PageRank and edge attributes containing the normalized edge weight.
+    *
+    * @tparam VD the original vertex attribute (not used)
+    * @tparam ED the original edge attribute (not used)
+    *
+    * @param graph the graph on which to compute PageRank
+    * @param tol the tolerance allowed at convergence (smaller => more accurate).
+    * @param resetProb the random reset probability (alpha)
+    * @param srcId the source vertex for a Personalized Page Rank (optional)
+    *
+    * @return the graph containing with each vertex containing the PageRank and each edge
+    *         containing the normalized weight.
+    */
+  def runUntilConvergenceWithOptions[VD: ClassTag, ED: ClassTag](
+                                                                  graph: Graph[VD, ED], tol: Double, resetProb: Double = 0.15,
+                                                                  srcId: Option[VertexId] = None): Graph[(Double,Int), Double] =
+  {
+    require(tol >= 0, s"Tolerance must be no less than 0, but got ${tol}")
+    require(resetProb >= 0 && resetProb <= 1, s"Random reset probability must belong" +
+      s" to [0, 1], but got ${resetProb}")
+
+    val personalized = srcId.isDefined
+    val src: VertexId = srcId.getOrElse(-1L)
+
+    // Initialize the pagerankGraph with each edge attribute
+    // having weight 1/outDegree and each vertex with attribute 1.0.
+    // TODO - modified to include # times vertex prog ran on each vertex
+    val pagerankGraph: Graph[(Double, Double, Int), Double] = graph
+      // Associate the degree with each vertex
+      .outerJoinVertices(graph.outDegrees) {
+      (vid, vdata, deg) => deg.getOrElse(0)
+    }
+      // Set the weight on the edges based on the degree
+      .mapTriplets( e => 1.0 / e.srcAttr )
+      // Set the vertex attributes to (initialPR, delta = 0)
+      .mapVertices { (id, attr) =>
+      if (id == src) (1.0, Double.NegativeInfinity,0) else (0.0, 0.0,0)
+    }
+      .cache()
+
+    // Define the three functions needed to implement PageRank in the GraphX
+    // version of Pregel
+    def vertexProgram(id: VertexId, attr: (Double, Double, Int), msgSum: Double): (Double, Double, Int) = {
+      val (oldPR, lastDelta, count) = attr
+      val newPR = oldPR + (1.0 - resetProb) * msgSum
+      (newPR, newPR - oldPR, count+1)
+    }
+
+    def personalizedVertexProgram(id: VertexId, attr: (Double, Double, Int),
+                                  msgSum: Double): (Double, Double, Int) = {
+      val (oldPR, lastDelta, count) = attr
+      var teleport = oldPR
+      val delta = if (src==id) resetProb else 0.0
+      teleport = oldPR*delta
+
+      val newPR = teleport + (1.0 - resetProb) * msgSum
+      val newDelta = if (lastDelta == Double.NegativeInfinity) newPR else newPR - oldPR
+      (newPR, newDelta, count+1)
+    }
+
+    def sendMessage(edge: EdgeTriplet[(Double, Double, Int), Double]) = {
+      if (edge.srcAttr._2 > tol) {
+        Iterator((edge.dstId, edge.srcAttr._2 * edge.attr))
+      } else {
+        Iterator.empty
+      }
+    }
+
+    def messageCombiner(a: Double, b: Double): Double = a + b
+
+    // The initial message received by all vertices in PageRank
+    val initialMessage = if (personalized) 0.0 else resetProb / (1.0 - resetProb)
+
+    // Execute a dynamic version of Pregel.
+    val vp = if (personalized) {
+      (id: VertexId, attr: (Double, Double, Int), msgSum: Double) =>
+        personalizedVertexProgram(id, attr, msgSum)
+    } else {
+      (id: VertexId, attr: (Double, Double, Int), msgSum: Double) =>
+        vertexProgram(id, attr, msgSum)
+    }
+
+    Pregel(pagerankGraph, initialMessage, activeDirection = EdgeDirection.Out)(
+      vp, sendMessage, messageCombiner)
+      .mapVertices((vid, attr) => (attr._1, attr._3))
+  }
+}
