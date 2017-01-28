4308a7eb2eda485ada7ba0c4895b6ca735ba14fe
Jose Cadena <jcadena@vbi.vt.edu>
2017-01-28 15:47:28 -0500

    added reader for APDM format and replaced hashmaps for arrays of 2d arrays

--- src/main/scala-2.10/org/saliya/graphxprimer/p3/Program2.scala
+++ src/main/scala-2.10/org/saliya/graphxprimer/p3/Program2.scala
@@ -46,7 +46,8 @@ object Program2 {
     val sc = new SparkContext(conf.setAppName("Multilinear (" + fname + ")").setMaster("local[*]"))
    //val sc = new SparkContext(conf.setAppName("Multilinear (" + fname + ")"))
 
-    val tup = createGraphFromFile(fname, n, k, r, sc, vertexStorageLevel, edgeStorageLevel)
+    //val tup = createGraphFromFile(fname, n, k, r, sc, vertexStorageLevel, edgeStorageLevel)
+    val tup = readAPDM(fname, k, r, sc, vertexStorageLevel, edgeStorageLevel)
     val g = tup._1.cache()
     val numColors = tup._2
 
@@ -59,19 +60,59 @@ object Program2 {
   }
 
   def testGraph(fname: String, k: PartitionID, r: PartitionID, numColors: PartitionID, graph: Graph[(PartitionID, Array[Array[PartitionID]], Array[Array[PartitionID]]), PartitionID]): Unit = {
-    val seed: VertexId = 10
-    val ret = colorfulGraphMotif(graph, k, r, seed)
-    println("\n*** Test for " + fname + " returned " + ret + " numcolors: " + numColors + " k: " + k)
+    val seed: Int = 10
+    //val iter: Int = Math.max(10, Math.log(graph.numVertices)).toInt
+    val iter: Int = 1
+    val alpha: Double = 0.15
+    var bestScore: Double = 0
+    for (i <- 0 until iter) {
+      val existenceTable = colorfulGraphMotif(graph, k, r, seed)
+      val bestScoreFromTable = getBestScoreFromTable(graph, alpha, existenceTable);
+      bestScore = Math.max(bestScoreFromTable, bestScore)
+    }
+    println("\n*** Test for " + fname + " returned " + bestScore)
+    //println("\n*** Test for " + fname + " returned " + ret + "k: " + k)
   }
 
-  def colorfulGraphMotif(graph: Graph[(Int, Array[Array[Int]], Array[Array[Int]]), Int], k: Int, r: Int, seed: Long): mutable.HashMap[VertexId, Array[Array[Boolean]]] = {
+  def getBestScoreFromTable(graph: Graph[(PartitionID, Array[Array[PartitionID]], Array[Array[PartitionID]]), PartitionID], alpha: Double, existenceTable: Array[Array[Array[Boolean]]]): Double = {
+    var bestScore: Double = 0
+    graph.vertices.foreach(v => {
+      val existenceForNode = existenceTable(v._1.toInt)
+      for (kPrime <- 1 until existenceForNode.length) {
+        for (rPrime <- existenceForNode(kPrime).indices) {
+          if (existenceForNode(kPrime)(rPrime)) {
+            val score = BJ(alpha, rPrime, kPrime)
+            bestScore = Math.max(bestScore, score)
+          }
+        }
+      }
+    })
+    bestScore
+  }
+
+  def BJ(alpha: Double, anomalousCount: Int, setSize: Int): Double = {
+    setSize * KL(anomalousCount.toDouble / setSize, alpha)
+  }
+
+  def KL(a: Double, b: Double): Double = {
+    assert(a >= 0 && a <= 1)
+    assert(b > 0 && b < 1)
+
+    // corner cases
+    if (a == 0) {
+      return Math.log(1 / (1 - b))
+    }
+    if (a == 1) {
+      return Math.log(1 / b)
+    }
+
+    a * Math.log(a / b) + (1 - a) * Math.log((1 - a) / (1 - b))
+  }
+
+
+  def colorfulGraphMotif(graph: Graph[(Int, Array[Array[Int]], Array[Array[Int]]), Int], k: Int, r: Int, seed: Long): Array[Array[Array[Boolean]]] = {
     // invalid input: k is negative
     if (k <= 0) throw new IllegalArgumentException("k must be a positive integer")
-    // trivial case: k = 1
-    // any color will do
-    //if (k == 1) {
-    //  return true
-    //}
 
     val random = new java.util.Random(seed)
     // (1 << k) is 2 raised to the kth power
@@ -85,19 +126,18 @@ object Program2 {
     // auxiliary variables to make a monomial multilinear if it has degree less than k
     val completionVariables: Array[Int] = new Array[Int](k - 1)
     completionVariables.indices.foreach(i => completionVariables(i) = random.nextInt(twoRaisedToK))
-
-    val totalSum: mutable.HashMap[VertexId, Array[Array[Int]]] = new mutable.HashMap[VertexId, Array[Array[Int]]]
-    graph.vertices.foreach(v => totalSum.put(v._1, Array.fill(k + 1, r + 1)(0)))
     val randomSeed: Long = random.nextLong
+    val totalSum: Array[Array[Array[Int]]] = Array.fill(graph.numVertices.toInt, k + 1, r + 1)(0)
 
     // TODO change 1 to twoRaisedToK
     println("Running for " + twoRaisedToK + " iterations")
     for (i <- 0 until twoRaisedToK) {
       val startTime: Long = System.currentTimeMillis
-      val s = evaluateCircuit(graph, randomAssignment, completionVariables, gf, k, r, i, randomSeed)
+      val finalGraph = evaluateCircuit(graph, randomAssignment, completionVariables, gf, k, r, i, randomSeed)
       // TODO Is there a more efficient way to merge?
-      for ((node, tableForNode) <- s) {
-        val tableTotalSum = totalSum(node)
+      finalGraph.vertices.cache().foreach(v => {
+        val tableForNode = v._2._3
+        val tableTotalSum = totalSum(v._1.toInt)
         for (kPrime <- 0 until (k + 1)) {
           for (rPrime <- 0 until (r + 1)) {
             //int weight = random.nextInt(gf.getFieldSize())
@@ -106,14 +146,14 @@ object Program2 {
             tableTotalSum(kPrime)(rPrime) = gf.add(tableTotalSum(kPrime)(rPrime), tableForNode(kPrime)(rPrime))
           }
         }
-      }
+      })
       val endTime: Long = System.currentTimeMillis
       println("Iteration " + i + ": Took " +  (endTime - startTime) / 1000.0 + " seconds")
     }
 
-    val decisionTable = new mutable.HashMap[VertexId, Array[Array[Boolean]]]
-    graph.vertices.foreach(v => totalSum.put(v._1, Array.fill(k + 1, r + 1)(0)))
-    for ((node, tableTotalSum) <- totalSum) {
+    val decisionTable: Array[Array[Array[Boolean]]] = Array.fill(graph.numVertices.toInt, k + 1, r + 1)(false)
+    totalSum.indices.foreach(node => {
+      val tableTotalSum = totalSum(node)
       val decisionTableForNode = decisionTable(node)
       for (kPrime <- 0 until (k + 1)) {
         for (rPrime <- 0 until (r + 1)) {
@@ -123,11 +163,11 @@ object Program2 {
           decisionTableForNode(kPrime)(rPrime) = tableTotalSum(kPrime)(rPrime) > 0
         }
       }
-    }
+    })
     decisionTable
   }
 
-  def evaluateCircuit(graph: Graph[(Int, Array[Array[Int]], Array[Array[Int]]), Int], randomAssignment: Array[Int], completionVariables: Array[Int], gf: GaloisField, k: Int, r: Int, iter: Int, randomSeed: Long): mutable.HashMap[VertexId, Array[Array[Int]]] ={
+  def evaluateCircuit(graph: Graph[(Int, Array[Array[Int]], Array[Array[Int]]), Int], randomAssignment: Array[Int], completionVariables: Array[Int], gf: GaloisField, k: Int, r: Int, iter: Int, randomSeed: Long): Graph[(PartitionID, Array[Array[PartitionID]], Array[Array[PartitionID]]), PartitionID] ={
     val random = new java.util.Random(randomSeed)
     val fieldSize = gf.getFieldSize
 
@@ -156,8 +196,10 @@ object Program2 {
       val nodePrize = v._2._1
       val dotProduct = randomAssignment(v._1.toInt) & iter
       val eigenvalue = if (Integer.bitCount(dotProduct) % 2 == 1) 0 else 1
-      rowOfTable(1)(nodePrize) = eigenvalue
-      rowOfExtendedTable(1)(nodePrize) = eigenvalue * cumulativeCompletionVariables(k - 1)
+      if (nodePrize <= r) {
+        rowOfTable(1)(nodePrize) = eigenvalue
+        rowOfExtendedTable(1)(nodePrize) = eigenvalue * cumulativeCompletionVariables(k - 1)
+      }
     })
 
 
@@ -175,10 +217,7 @@ object Program2 {
 //    var circuitSum = 0
 //    products.indices.foreach(i => circuitSum = gf.add(circuitSum, products(i)._2))
 //    circuitSum
-
-    val extendedTable = new mutable.HashMap[VertexId, Array[Array[Int]]]
-    finalGraph.vertices.cache().foreach(v => extendedTable.put(v._1, v._2._3))
-    extendedTable
+    finalGraph
   }
 
   def vprogWrapper(k: Int, r: Int, random: java.util.Random, fieldSize: Int, gf: GaloisField, cumulativeCompletionVariables: Array[Int]): (VertexId, (PartitionID, Array[Array[PartitionID]], Array[Array[PartitionID]]), mutable.HashMap[PartitionID, (Array[Array[PartitionID]], Array[Array[PartitionID]])]) => (PartitionID, Array[Array[PartitionID]], Array[Array[PartitionID]]) = (vertexId: VertexId, value: (Int, Array[Array[Int]], Array[Array[Int]]), message: scala.collection.mutable.HashMap[Int, (Array[Array[Int]], Array[Array[Int]])]) =>  {
@@ -264,7 +303,7 @@ object Program2 {
 
       if (!line.startsWith("#") && mode != 2){
         val splits = line.split(" ")
-        if (mode == 0){
+        if (mode == 0) {
           val vertexId = splits(0).toInt
           val prize = if (splits(1).toFloat < alpha) 1 else 0
           colors.add(prize)
@@ -283,6 +322,76 @@ object Program2 {
     val edgesRDD: RDD[Edge[Int]] = sc.parallelize(edges).persist(esl)
 
     (Graph(verticesRDD, edgesRDD, defaultVertex), colors.size)
+  }
+
+  def readAPDM(f:String, k: Int, r: Int, sc: SparkContext, vsl: StorageLevel, esl: StorageLevel): (Graph[(PartitionID, Array[Array[PartitionID]], Array[Array[PartitionID]]), PartitionID], PartitionID) ={
+
+    val lines: Array[String] = Source.fromFile(f).getLines.toArray
+
+    // read number of nodes and edges
+    var lineNumber: Int = 0
+    var line = lines(lineNumber)
+    while (! line.startsWith("SECTION1") && lineNumber < lines.length) {
+      lineNumber += 1
+      line = lines(lineNumber)
+    }
+    lineNumber += 1
+    line = lines(lineNumber)
+    val n = line.stripLineEnd.split(" ").last.toInt
+    lineNumber += 1
+    line = lines(lineNumber)
+    val m = line.stripLineEnd.split(" ").last.toInt
+
+    // initialize data structures for nodes and edges
+    val vertices = new Array[(Long, (Int, Array[Array[Int]], Array[Array[Int]]))](n)
+    val edges = new Array[Edge[Int]](2 * m) // double count because Scala uses directed edges
+
+    // read nodes
+    lineNumber += 1
+    line = lines(lineNumber)
+    while (! line.startsWith("SECTION2") && lineNumber < lines.length) {
+      lineNumber += 1
+      line = lines(lineNumber)
+    }
+    lineNumber += 1
+    line = lines(lineNumber)
+    lineNumber += 1
+    line = lines(lineNumber)
+    while (! line.startsWith("END")) {
+      val tokens = line.stripLineEnd.split(" ")
+      val vertexId = tokens(0).toInt
+      val prize = tokens(1).toDouble.toInt
+      vertices(vertexId) = (vertexId.toLong, (prize, Array.fill(k+2, r+1)(0), Array.fill(k+2, r+1)(0)))
+      lineNumber += 1
+      line = lines(lineNumber)
+    }
+    // read edges
+    lineNumber += 1
+    line = lines(lineNumber)
+    lineNumber += 1
+    line = lines(lineNumber)
+    lineNumber += 1
+    line = lines(lineNumber)
+    lineNumber += 1
+    line = lines(lineNumber)
+    var numEdges = 0
+    while (! line.startsWith("END")) {
+      val tokens = line.stripLineEnd.split(" ")
+      val u = tokens(0).toInt
+      val v = tokens(1).toInt
+      edges(numEdges) = Edge(u.toInt, v.toInt, 1)
+      numEdges += 1
+      edges(numEdges) = Edge(v.toInt, u.toInt, 1)
+      numEdges += 1
+      lineNumber += 1
+      line = lines(lineNumber)
+    }
+    val defaultVertex = (-1, Array(Array(-1)), Array(Array(-1)))
+
+    val verticesRDD: RDD[(VertexId, (Int, Array[Array[Int]], Array[Array[Int]]))] = sc.parallelize(vertices).persist(vsl)
+    val edgesRDD: RDD[Edge[Int]] = sc.parallelize(edges).persist(esl)
+
+    (Graph(verticesRDD, edgesRDD, defaultVertex), 0)
   }
 
   def log2(x: Int): Int = {
